c4ef5ed5809c28a211e3c13ec3210f1118393a01
Diego <sondiegolt@gmail.com>
2025-04-10 19:26:34 +0200

    2

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { getCsrfToken, getProviders } from "next-auth/react";
+import { getCsrfToken } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import {
